830a6adf7462b6a4673a6e76cc4306af1a8d0d81
GuiBourdin <50623880+guibourdin@users.noreply.github.com>
2020-06-23 16:13:08 -0400

    Update Matchups_InSitu_Satellite.m

--- Matchups_InSitu_Satellite.m
+++ Matchups_InSitu_Satellite.m
@@ -11,9 +11,9 @@ function insitu_remote_match = Matchups_InSitu_Satellite(data, pathOC, varargin)
 %         - lon spectrum <Nx1 double> longitude in decimal degrees
 %         - any other variable M x <Nx1 double>
 %   - pathOC: string of path to data folder
-%   - optional argument: structure with three fields:
-%       - (1) variable to extract: <1xM cellstr> exact variable matching OC file variables
-%           (default = all OC variables)
+%   - optional argument:
+%       - (1) variable to extract: <1xM cellstr> exact variable matching geophysical variables
+%           (default = all variables)
 %       - (2) area match: size of square area around in-situ measurement (in pixel)
 %           (default = 5; for 5x5 pixel area)
 %       - (3) time interval before and after satellite overpass (in minutes)
@@ -21,50 +21,61 @@ function insitu_remote_match = Matchups_InSitu_Satellite(data, pathOC, varargin)
 %       - (4) plot: logical
 %           (default = false)
 %       - (5) variable to plot: <1x2 cellstr> string of exact variable to plot
-%       matching OC variable (1) and in-situ data variable (2)
+%           matching OC variable (1) and in-situ data variable (2)
+%            (default = plot == false)
+%       - (6) image name list to match: <Nx6 struct> structure listing
+%           image names to match with given by: dir('*.nc')
+%            (default = all *.nc files in folder)
 %
 % OUTPUT:
 %   - data_out: <NxM table> of matchups
-% example: insitu_remote_match = Matchups_InSitu_Satellite(par, pathOC, var_to_extract, 5, 180, true, {'par', 'ipar'});
+% example: insitu_remote_match = Matchups_InSitu_Satellite(par, pathOC, var_to_extract, 5, 180, true, {'par', 'ipar'}, selseg);
 
 cd(pathOC);
 % list NC files in folder
-ncOCfiles = dir('*.nc');
+if nargin < 8
+    ncOCfiles = dir('*.nc');
+else
+    ncOCfiles = varargin{6};
+end
 NOCfiles = length(ncOCfiles); 
-NCinf = ncinfo(ncOCfiles(1).name,'/geophysical_data/');
-remote_varnames = fullfile({NCinf.Variables.Name});
 
 if nargin < 1
-    error('missing in-situ data & path to OC files')
+    error('missing in-situ data & image list')
 elseif nargin < 2
-    error('missing path to ')
+    error('missing path to image')
 elseif nargin < 3
     varargin{2} = 5;
     varargin{3} = 180;
     varargin{4} = false;
+    NCinf = ncinfo(ncOCfiles(1).name,'/geophysical_data/');
+    remote_varnames = fullfile({NCinf.Variables.Name});
     varargin{1} = remote_varnames;
-    warning('missing variable to extract; default = all OC variables')
+    warning('missing variable to extract; default = all variables')
     warning('missing matching area; default = 5x5 pixels')
     warning('missing matching time interval; default = 3h')
-    warning('plot = false')
+    warning('plot set to false')
 elseif nargin < 4
     varargin{2} = 5;
     varargin{3} = 180;
     varargin{4} = false;
     warning('missing matching area; default = 5x5 pixels')
     warning('missing matching time interval; default = 180 minutes')
-    warning('plot = false')
+    warning('plot set to false')
 elseif nargin < 5
     varargin{3} = 180;
     varargin{4} = false;
     warning('missing matching time interval; default = 180 minutes')
-    warning('plot = false')
+    warning('plotset to false')
 elseif nargin < 6
     varargin{4} = false;
-    warning('plot = false')
-elseif nargin < 7
-    warning('missing variable to plot: plot = false')
-else
+    warning('plot set to false')
+elseif all(nargin < 7 & varargin{4})
+    warning('missing variable to plot: plot set to false')
+elseif isempty(varargin{5})
+    varargin{4} = false;
+    warning('missing variable to plot; plot set to false')
+elseif all(nargin > 6 & ~isempty(varargin{5}))
     varargin{5}{1} = ['insitu_' varargin{5}{1}];
     varargin{5}{2} = ['remote_' varargin{5}{2}];
 end
@@ -93,7 +104,7 @@ for i = 1:NOCfiles
     sec = floor(ncread(ncOCfiles(i).name,'/scan_line_attributes/msec')/1000)-(h*3600+minu*60);
     datetimeOC = datetime(ye,month(D),day(D),h,minu,sec);
     % identify Tara lat lon for satellite overpass time
-    t_match = find(data.dt > datetimeOC(1) - varargin{3} & data.dt < datetimeOC(end) + varargin{3});
+    t_match = find(data.dt > datetimeOC(1) - varargin{3}-360 & data.dt < datetimeOC(end) + varargin{3}+360);
     sel_datalon = data.lon(t_match);
     if any(t_match) % if time match
         latOCini = ncread(ncOCfiles(i).name,'/navigation_data/latitude');
@@ -126,10 +137,13 @@ for i = 1:NOCfiles
             raddisID = acos(sin(median(data.lat(t_match))*pi/180).*sin(latOC*pi/180)...
                 +cos(median(data.lat(t_match))*pi/180).*cos(latOC*pi/180)...
                 .*cos(abs(median(data.lon(t_match))*pi/180-lonOC*pi/180)));%calculate the distance in radians with tsg at for each iteration
-            [~,n_indices]=sort(raddisID(:),'ascend');    % sort the distances to find closest match area
+            [~,n_indices] = sort(raddisID(:),'ascend');    % sort the distances to find closest match area
             K = n_indices(1:varargin{2}^2,:);
             kmdis = rad2km(raddisID(K));% * 3437.74677 (nautical miles); % calculate the closest distance in km
-            if min(kmdis) < varargin{2}*2 % matchup if closest match is closer than twice the distance between center and furthest pixel
+            reso_sat = rad2km(acos(sin(latOCini(K(1))*pi/180).*sin(latOCini(K(1)+1)*pi/180)...
+                +cos(latOCini(K(1))*pi/180).*cos(latOCini(K(1)+1)*pi/180)...
+                .*cos(abs(lonOCini(K(1))*pi/180-lonOCini(K(1)+1)*pi/180))));%calculate the distance in km
+            if min(kmdis) < 1.2*reso_sat % matchup if closest match is closer than 1.2 * satellite resolution
                 for j = 1:size(data,2)
                     insitu_remote_match{i,j} = data.(data.Properties.VariableNames{j})(t_match);
                 end
@@ -210,8 +224,8 @@ for i = 1:NOCfiles
                         pat = pat(idpat,:);
                         patf = [pat; pat(1,:)];
                         m_patch(patf(:,1),patf(:,2), [0.85 0.85 0.85],'FaceAlpha',0.6);
-                        ylabel(cb2, strrep(sprintf('%s (%s)', varargin{5}{1}, data.Properties.VariableUnits{id_plotinsitu}),'_',' '));
-                        if isempty(data.Properties.VariableUnits{id_plotinsitu})
+                        ylabel(cb2, strrep(sprintf('%s (%s)', varargin{5}{1}, unit{id_plotinsitu}),'_',' '));
+                        if isempty(unit{id_plotinsitu})
                             warning('In-situ unit missing')
                         end
                         %%%%%%%%%%% change marker size
@@ -221,21 +235,21 @@ for i = 1:NOCfiles
                             100,insitu_remote_match{i,id_plotinsitu}(sel_match_dt),'filled', 'MarkerEdgeColor', 'k', 'LineWidth', 0.5);
                         m_scatter(insitu_remote_match{i,ilon}(sel_match_dt),insitu_remote_match{i,ilat}(sel_match_dt),...
                             100,insitu_remote_match{i,id_plotinsitu}(sel_match_dt),'filled');
-                        caxis(ax, [min(min(remote{id_plotremote(size(data,2)+5:end-1)}))...
-                            max(max(remote{id_plotremote(size(data,2)+5:end-1)}))]);
-                        caxis(ax2, [min(insitu_remote_match{i,id_plotinsitu})...
-                            max(insitu_remote_match{i,id_plotinsitu})]);
+%                         caxis(ax, [min(min(remote{id_plotremote(size(data,2)+5:end-1)}))...
+%                             max(max(remote{id_plotremote(size(data,2)+5:end-1)}))]);
+%                         caxis(ax2, [min(insitu_remote_match{i,id_plotinsitu})...
+%                             max(insitu_remote_match{i,id_plotinsitu})]);
                         selday_data = data.dt > median(insitu_remote_match{i,idt}) - days(0.5)...
                             & data.dt < median(insitu_remote_match{i,idt}) + days(0.5);
                         fhv=figure(2);
                         set(fhv,'units','normalized','outerposition',[0.5 0.025 0.5 0.975]);
-                        scatter(data.dt(selday_data),data.par(selday_data),7,'filled'); hold on
+                        scatter(data.dt(selday_data),data.(varargin{5}{1}(8:end))(selday_data),7,'filled'); hold on
                         scatter(insitu_remote_match{i,idt}(sel_match_dt),insitu_remote_match{i,id_plotinsitu}(sel_match_dt),10,'r','filled');
                         y_lim = ylim();
                         area([min(insitu_remote_match{i,size(data,2)+2}) max(insitu_remote_match{i,size(data,2)+2})],...
                             [y_lim(2), y_lim(2)], y_lim(1),'FaceColor', [0.9 0.3 0.3], 'FaceAlpha', 0.3, 'EdgeColor', 'none');
-                        ylabel(strrep(sprintf('%s (%s)', varargin{5}{1}, data.Properties.VariableUnits{id_plotinsitu}),'_',' '));
-                        if isempty(data.Properties.VariableUnits{id_plotinsitu})
+                        ylabel(strrep(sprintf('%s (%s)', varargin{5}{1}, unit{id_plotinsitu}),'_',' '));
+                        if isempty(unit{id_plotinsitu})
                             warning('In-situ unit missing')
                         end
                         title('Validate match-up (press q) / skip matchup (press s) ');
@@ -262,6 +276,10 @@ for i = 1:NOCfiles
 end
 insitu_remote_match(all(cellfun(@isempty,insitu_remote_match),2),:) = [];
 insitu_remote_match = cell2table(insitu_remote_match,'VariableNames', varnames);
+if exist('unit','var')
+    unit(cellfun(@isempty,unit))={'na'};
+    insitu_remote_match.Properties.VariableUnits = unit;
+end
 tmp = matlab.desktop.editor.getActive; cd(fileparts(tmp.Filename));
 save('insitu_remote_match.mat','insitu_remote_match')
 end
